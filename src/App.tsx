5aa43a7d7518239e0c20658f79fba470bc54ba0c
malinasdfghjkl <malin.marques@gmail.com>
2022-10-21 19:09:34 +0200

    Revert "Merge pull request #5 from armada-ths/Mehir/Login-Register-Header"
    This reverts commit a07be3c0a5d20be153290b59dd968a17b5f252b6, reversing
    changes made to b2a9ef58f9d84e79162a4f45489fa1e0e4a8dfce.

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import HomePresenter from "./presenters/HomePresenter";
 import EventsPresenter from "./presenters/EventsPresenter";
-import RegisterPresenter from "./presenters/RegisterPresenter";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" element={<HomePresenter placeholder="home" />} />
-        		<Route path="/events" element={<EventsPresenter placeholder="events" />} />
-				<Route path="/register" element={<RegisterPresenter />} />
+        <Route path="/events" element={<EventsPresenter placeholder="events" />} />
 			</Routes>
 		</Router>
 	);
--- src/presenters/RegisterPresenter.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from "react";
-import RegisterView from "../views/RegisterView";
-
-const RegisterPresenter: FC = () => {
-    return <RegisterView title="Register" />  
-}
-
-export default RegisterPresenter;

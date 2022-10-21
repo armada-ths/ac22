fa0329808731f10270b489eb06291a1f4d9d8881
Rakin Ali <rakinali00@gmail.com>
2022-10-21 20:28:13 +0200

    Dana's attempt
    Dana tried to help me with the styling. He centered everything for me. Now I have to fix the rest.
    
    GG Dana
    
    Co-Authored-By: Dana Ismail <54773040+DanaIso@users.noreply.github.com>

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -10,25 +10,45 @@ interface Props {
 	placeholder?: string;
 }
 
+/*
+ Dependencies: Armada Logo, Dashboard Icon, Events Icon
+	@returns: Sidebar component
+
+	This component is the sidebar navigation which is seen on the homepage and eventspage
+	
+	
+*/
+
 const Sidebar: FC<Props> = () => {
 	const [active, setActive] = useState<boolean>(false);
-	
+
 	return (
-		<nav className="sidebarFlex">
-			<img className="armada-icon" src={armada_logo} alt="Armada Logo" />
+		<div className="sidebarFlex">
+			<div className="sidebar-LinkButton">
+				<img className="armada-icon" src={armada_logo} alt="Armada Logo" />
+			</div>
 			<div className="sidebar-icons-flex">
-				<div className="sidebar-LinkButton">
-					<Link to="/" className="button-link" onClick={() => setActive(!active)}>
-						<img src={dashboard_grey} alt="dashboard" className="navbar-icon"/>
+				<div className="sidebar-LinkButton another-sidebar-LinkButton">
+					<Link
+						to="/"
+						className="button-link"
+						onClick={() => setActive(!active)}
+					>
+						<img src={dashboard_grey} alt="dashboard" className="navbar-icon" />	
 					</Link>
 				</div>
-				<div className="sidebar-LinkButton">
-					<Link to="/events" className={active ? "button-active" : "button-inactive"} onClick={() => setActive(!active)}>
-						<img src={events_grey} alt="dashboard" className="navbar-icon"/>
+
+				<div className="sidebar-LinkButton another-sidebar-LinkButton">
+					<Link
+						to="/events"
+						className={"button-link"}
+						onClick={() => setActive(!active)}
+					>
+						<img src={events_grey} alt="dashboard" className="navbar-icon" />
 					</Link>
 				</div>
 			</div>
-		</nav>
+		</div>
 	);
 };
 

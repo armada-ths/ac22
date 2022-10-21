ea000575df7ba9aa615bba7f93d0076cdf1b5d91
Yash Dhanore <yashdhanore@gmail.com>
2022-10-21 21:04:14 +0200

    password input field regex validation left

--- src/components/ACInput/ACInput.tsx
+++ src/components/ACInput/ACInput.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import "./ACInput.css";
 
 interface Props {
@@ -7,9 +7,46 @@ interface Props {
 }
 
 const ACInput: FC<Props> = ({ type, placeholder }) => {
+  const [details, setDetails] = useState({
+    username: "",
+    error: false,
+  });
+
+  const handleUsernameChange = (e: any) => {
+    console.log("handleUsernameChange()");
+    const target = e.target;
+    setDetails({
+      username: target.value,
+      error: target.validationMessage,
+    });
+  };
+
+  const [validInput, setValidInput] = useState<boolean>(true);
+
+  const emailPattern = "^[A-Za-z0-9._%+-]+@kth.se$";
+  const passwordPattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)[a-zA-Zd]{8,}$";
+
+  const handleEmailChange = (e: any) => {
+    if (e.target.validationMessage) {
+      console.log(
+        e.target.validationMessage === "Please match the requested format."
+      );
+      setValidInput(false);
+    } else {
+      setValidInput(true);
+    }
+  };
+
   return (
-    <div className="container">
-      <input className="ac-input" placeholder={placeholder} type={type} />
+    <div className={validInput ? "container" : "incorrect-container"}>
+      <input
+        className="ac-input"
+        placeholder={placeholder}
+        type={type}
+        pattern={type == "email" ? emailPattern : passwordPattern}
+        onChange={handleEmailChange}
+      />
+      <div className="invalid-feedback d-block">{details.error}</div>
     </div>
   );
 };

fc9c192ea7c87bd59b557c6476b14f0a95e5eee0
ninaayad <nina_9902@hotmail.se>
2022-10-28 15:07:41 +0200

    Company card fixes
    Fixed the css of the company card component and the ticket state component, now I'm not using any margins to make everything stick to its place but only using flex since I think it is better to do it that way.
    
    Also fixed some sizing to make it more similar to the figma model.
    
    Added a component AllCards that is used only to showcase, should be removed if merged to main!

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,15 @@ import HomePresenter from "./presenters/HomePresenter";
 import EventsPresenter from "./presenters/EventsPresenter";
 import RegisterPresenter from "./presenters/RegisterPresenter";
 
-import CompanyCard from "./components/CompanyCard/CompanyCard"; // Remove
+import AllCards from './components/AllCards' // Remove
 
 /**
- * You can use the code below to test the buttons
+ * You can use the code below to test the cards
  * 
  * Remove this comment before merging (if merging) to main
- * and also remove the import of CompanyCard
- * and the paths available, none-available and received
+ * and also remove the import of AllCards
+ * and the file src/components/AllCards.tsx
+ * and the path "cards"
  * these were only set up to try out the component
  */
 function App() {
@@ -23,30 +24,7 @@ function App() {
 				<Route path="/" element={<HomePresenter placeholder="home" />} />
         		<Route path="/events" element={<EventsPresenter placeholder="events" />} />
 				<Route path="/register" element={<RegisterPresenter />} />
-				<Route path="/available" element={<CompanyCard 
-					image="var(--armada-green)"
-					companyName="Volvo Automotive"
-					tags={["workshop", "mingle"]}
-					starred={true}
-					onStar={() => console.log("on-star")}
-					ticketState={"available"}
-					receivedTickets={0} />} />
-				<Route path="/none-available" element={<CompanyCard 
-					image="var(--armada-green)"
-					companyName="Volvo Automotive"
-					tags={["workshop", "mingle"]}
-					starred={true}
-					onStar={() => console.log("on-star")}
-					ticketState={"none available"}
-					receivedTickets={0} />} />
-				<Route path="/received" element={<CompanyCard 
-					image="var(--armada-green)"
-					companyName="Volvo Automotive"
-					tags={["workshop", "mingle"]}
-					starred={true}
-					onStar={() => console.log("on-star")}
-					ticketState={"received"}
-					receivedTickets={10} />} />
+				<Route path="/cards" element={<AllCards placeholder="" />} />
 			</Routes>
 		</Router>
 	);

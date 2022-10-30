6f65614c97870cd9bbb6f28dee72d05e2265e67a
paklas99 <zitta@live.se>
2022-10-30 22:53:32 +0100

    Added some errorhandling

--- src/components/AuthButton/LoginButton.tsx
+++ src/components/AuthButton/LoginButton.tsx
@@ -12,12 +12,30 @@ interface Props {
   buttonType?: "button" | "submit" | "reset" | undefined;
   buttonText: string;
   active: boolean;
-  //title: string;
 }
 
 const LoginButton: FC<Props> = ({ buttonType, buttonText, active}) => {
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [emailError, setEmailError] = React.useState("");
+  const [passwordError, setPasswordError] = React.useState("");
+
+  const handleLogin = () => {
+    //clearErrors();
+    signInWithEmailAndPassword(auth, username, password)
+    .catch((err) => {
+        switch(err.code) {
+            case "auth/invalid-email":
+            case "auth/user-disabled":
+            case "auth/user-not-found":
+                setEmailError(err.message);
+                break;
+            case "auth/wrong-password":
+                setPasswordError(err.message);
+                break;
+        }
+    });
+  } 
   
   return (
     <div>
@@ -33,7 +51,7 @@ const LoginButton: FC<Props> = ({ buttonType, buttonText, active}) => {
           value={password}
           onChange={setPassword} 
         /> 
-    <button disabled={!active} type={buttonType} className="armada-button" onClick={() => signInWithEmailAndPassword(auth, username, password)} >
+    <button disabled={!active} type={buttonType} className="armada-button" onClick={handleLogin} >
       {buttonText}
     </button>
     </div>
--- src/components/AuthButton/RegButton.tsx
+++ src/components/AuthButton/RegButton.tsx
@@ -18,6 +18,29 @@ interface Props {
 const RegButton: FC<Props> = ({ buttonType, buttonText, active}) => {
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [emailError, setEmailError] = React.useState("");
+  const [passwordError, setPasswordError] = React.useState("");
+
+  const clearErrors = () => {
+    setEmailError('');
+    setPasswordError('');
+  }
+
+  const handleSignUp = () => {
+    clearErrors();
+    createUserWithEmailAndPassword(auth, username, password)
+    .catch((err) => {
+      switch(err.code) {
+        case "auth/email-already-in-use":
+        case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+        case "auth/weak-password":
+            setPasswordError(err.message);
+            break;
+      }
+    });
+  }
 
   return (
     <div>
@@ -32,11 +55,11 @@ const RegButton: FC<Props> = ({ buttonType, buttonText, active}) => {
           placeholder="Password"
           value={password}
           onChange={setPassword} //test
+    
         /> 
-    <button disabled={!active} type={buttonType} className="armada-button" onClick={() => createUserWithEmailAndPassword(auth, username, password)} >
+    <button disabled={!active} type={buttonType} className="armada-button" onClick={handleSignUp} >
       {buttonText}
     </button>
-    
     </div>
   );
 };

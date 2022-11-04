c0379393009ce478b4be293500ae0376c8977d76
malinasdfghjkl <malin.marques@gmail.com>
2022-11-04 18:15:48 +0100

    inget

--- src/components/DropdownMenu/DropdownMenu.tsx
+++ src/components/DropdownMenu/DropdownMenu.tsx
@@ -4,7 +4,7 @@ import { DropdownItem } from "./DropdownItem";
 import "./DropdownMenu.css";
 interface Props {
   title: string,
-  items: any,
+  items: DropdownItem[],
   selectedItems: DropdownItem[] | DropdownItem,
   onChange: (selected: DropdownItem[] | DropdownItem) => (void),
   multiSelect: boolean;
@@ -20,7 +20,7 @@ const DropdownMenu: FC<Props> = ({
   // const styling = {
   //   control: (styles: any) => ({...styles, borderColor: "#2BDBA0", borderRadius: "0.5rem", backgroundColor: "#2BDBA0", color: "white", width: "100%", height: "3rem", fontSize: "1.5rem", fontWeight: "bold", display: "flex", alignItems: "center", justifyContent: "center"}),
   // }
- 
+
   return (
     <div className="dropdown-container">
       <div className="dropdown-title">{title}</div>

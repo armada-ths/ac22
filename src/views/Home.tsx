56492ba08cd94c40fecc9881603763b9ef33aa43
malinasdfghjkl <malin.marques@gmail.com>
2022-11-07 11:54:14 +0100

    set-up wip

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,14 @@
 import React, { FC } from "react";
-
+import Countdown from "../components/Countdown/Countdown";
 interface Props {
 }
 
 const HomeViewer: FC<Props> = () => {
-	return <div>Home page</div>;
+	return <div>Home page
+		<div>
+			<Countdown today={Date.now()}/>
+		</div>
+	</div>;
 };
 
 export default HomeViewer;

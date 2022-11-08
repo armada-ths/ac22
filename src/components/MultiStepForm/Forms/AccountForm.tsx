3628edaa1ea9bf02bb503b005d86340eb9932cb4
Yash Dhanore <yashdhanore@gmail.com>
2022-11-08 19:48:18 +0100

    repeat password button done

--- src/components/MultiStepForm/Forms/AccountForm.tsx
+++ src/components/MultiStepForm/Forms/AccountForm.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import ACInput from "../../ACInput/ACInput";
 import "./Forms.css";
 
@@ -12,6 +12,20 @@ interface Props extends AccountData {
 }
 
 const AccountForm: FC<Props> = ({ email, password, updateField }) => {
+  const [validInput, setValidInput] = useState<boolean>(true);
+  const [active, setActive] = useState<boolean>(false);
+  const [repeatPassword, setRepeatPassword] = React.useState("");
+  const [place, setPlace] = useState("Repeat Password");
+
+  useEffect(() => {
+    if (repeatPassword === password) {
+      console.log("Passwords match");
+      setValidInput(true);
+    } else {
+      setValidInput(false);
+    }
+  }, [repeatPassword, password]);
+
   return (
     <div className="form-content">
       <h2>Get Registered</h2>
@@ -27,12 +41,37 @@ const AccountForm: FC<Props> = ({ email, password, updateField }) => {
         value={password}
         onChange={(e) => updateField({ password: e })}
       />
-      <ACInput
-        type="password"
-        placeholder="Repeat Password"
-        value={password}
-        onChange={(e) => updateField({ password: e })}
-      />
+      <div>
+        <div
+          className={validInput ? "InputHeader" : "InputHeaderWrong"}
+          style={{ color: validInput && !active ? "#0F1322" : "" }}
+        >
+          {active ? "Repeat password" : ""}
+        </div>
+        <div
+          className={validInput ? "container" : "incorrect-container"}
+          style={{ border: validInput && !active ? "1.5px solid #0F1322" : "" }}
+        >
+          <input
+            className="ac-input"
+            placeholder={place}
+            type="password"
+            value={repeatPassword}
+            style={{
+              backgroundColor: !validInput && !active ? "#FFE6E6" : "",
+            }}
+            onChange={(e) => setRepeatPassword(e.target.value)}
+            onFocus={() => {
+              setActive(true);
+              setPlace("");
+            }}
+            onBlur={() => {
+              setActive(false);
+              setPlace("Repeat Password");
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };

7c823b9f0596c526a364bc24d5955ae400485119
Yash Dhanore <yashdhanore@gmail.com>
2022-11-08 18:24:31 +0100

    small fix

--- src/components/MultiStepForm/Forms/AccountForm.tsx
+++ src/components/MultiStepForm/Forms/AccountForm.tsx
@@ -1,25 +1,40 @@
-import React, { FC, useState } from 'react'
+import React, { FC } from "react";
 import ACInput from "../../ACInput/ACInput";
 import "./Forms.css";
 
 interface AccountData {
-    email: string,
-    password: string,
+  email: string;
+  password: string;
 }
 
 interface Props extends AccountData {
-    updateField: (data: Partial<AccountData>) => void
+  updateField: (data: Partial<AccountData>) => void;
 }
 
 const AccountForm: FC<Props> = ({ email, password, updateField }) => {
   return (
-    <div className='form-content'>
-        <h2>Get Registered</h2>
-        <ACInput type="email" placeholder="Email" value={email} onChange={(e) => updateField({ email: e.target.value })}/>
-        <ACInput type="password" placeholder="Password" value={password} onChange={(e) => updateField({ password: e.target.value })}/>
-        <ACInput type="password" placeholder="Repeat Password" value={password} onChange={(e) => updateField({ password: e.target.value })}/>
+    <div className="form-content">
+      <h2>Get Registered</h2>
+      <ACInput
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => updateField({ email: e })}
+      />
+      <ACInput
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => updateField({ password: e })}
+      />
+      <ACInput
+        type="password"
+        placeholder="Repeat Password"
+        value={password}
+        onChange={(e) => updateField({ password: e })}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default AccountForm
+export default AccountForm;
--- src/components/MultiStepForm/Forms/StudyForm.tsx
+++ src/components/MultiStepForm/Forms/StudyForm.tsx
@@ -1,29 +1,54 @@
-import React, { FC, useState } from 'react'
-import ACInput from '../../ACInput/ACInput'
+import React, { FC } from "react";
+import ACInput from "../../ACInput/ACInput";
 import "./Forms.css";
 
 type AccountData = {
-  studyProgramme: string,
-  gender: string,
-  yearOfStudy: string,
-  completionYear: string
-}
+  studyProgramme: string;
+  gender: string;
+  yearOfStudy: string;
+  completionYear: string;
+};
 
 interface Props extends AccountData {
-  updateField: (data: Partial<AccountData>) => void
+  updateField: (data: Partial<AccountData>) => void;
 }
 
-const StudyForm: FC<Props> = ({studyProgramme, gender, yearOfStudy, completionYear, updateField}) => {
-
+const StudyForm: FC<Props> = ({
+  studyProgramme,
+  gender,
+  yearOfStudy,
+  completionYear,
+  updateField,
+}) => {
   return (
-    <div className='form-content'>
-        <h2>Education</h2>
-        <ACInput type="" placeholder="Study Programme" value={studyProgramme} onChange={(e) => updateField({ studyProgramme: e.target.value })}/>
-        <ACInput type="" placeholder="Gender" value={gender} onChange={(e) => updateField({ gender: e.target.value })}/>
-        <ACInput type="" placeholder="Year of study" value={yearOfStudy} onChange={(e) => updateField({ yearOfStudy: e.target.value })}/>
-        <ACInput type="" placeholder="Year of Completion" value={completionYear} onChange={(e) => updateField({ completionYear: e.target.value })}/>
+    <div className="form-content">
+      <h2>Education</h2>
+      <ACInput
+        type=""
+        placeholder="Study Programme"
+        value={studyProgramme}
+        onChange={(e) => updateField({ studyProgramme: e })}
+      />
+      <ACInput
+        type=""
+        placeholder="Gender"
+        value={gender}
+        onChange={(e) => updateField({ gender: e })}
+      />
+      <ACInput
+        type=""
+        placeholder="Year of study"
+        value={yearOfStudy}
+        onChange={(e) => updateField({ yearOfStudy: e })}
+      />
+      <ACInput
+        type=""
+        placeholder="Year of Completion"
+        value={completionYear}
+        onChange={(e) => updateField({ completionYear: e })}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default StudyForm
+export default StudyForm;

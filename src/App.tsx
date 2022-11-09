9ddeef333a927344c4a61c6ee756d1548c4ec8a2
ninaayad <nina_9902@hotmail.se>
2022-11-09 19:26:48 +0100

    Comments deleted
    Deleted comments from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePresenter from "./presenters/HomePresenter";
 import EventsPresenter from "./presenters/EventsPresenter";
 import RegisterPresenter from "./presenters/RegisterPresenter";
-import CompanyView from "./views/CompanyView/CompanyView"; // Remove
-import TutorialPresenter from "./presenters/TutorialPresenter";
-import LoginPresenter from "./presenters/LoginPresenter";
 
-import { dummyCompanies, dummyTickets } from './models/dummyConstant' // Remove
-
-/**
- * You can use the code below to test the view
- * 
- * Remove this comment before merging (if merging) to main
- * and also remove the import of CompanyView and dummyConstant
- * and the path to company-view
- * this was only set up to try out the component
- * 
- * ALSO REMOVE ALL CARDS!!
- */
 function App() {
 	return (
 		<Router>
@@ -28,13 +13,6 @@ function App() {
 				<Route path="/" element={<HomePresenter placeholder="home" />} />
         		<Route path="/events" element={<EventsPresenter placeholder="events" />} />
 				<Route path="/register" element={<RegisterPresenter />} />
-				<Route path="/company" element={<CompanyView
-					companies={dummyCompanies}
-					currentCompany={0}
-					onStar={() => console.log("on-star")}
-					availableTickets={dummyTickets} />} />
-				<Route path="/tutorial" element={<TutorialPresenter />} />
-				<Route path="/login" element={<LoginPresenter />} />
 			</Routes>
 		</Router>
 	);

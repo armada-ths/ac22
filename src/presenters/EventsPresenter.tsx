fd85030b684510b8b56fc69d025b3b47f6d2e65d
Rakin Ali <rakinali00@gmail.com>
2022-11-11 18:33:28 +0100

    Quick Sidebar
    To however is going to add Sidebar on top of their views --> Look at this PR. Mainly look at presenters EventPresenter and HomePresenter.
    
    @malinasdfghjkl  Let me know when you're done with your PR

--- src/presenters/EventsPresenter.tsx
+++ src/presenters/EventsPresenter.tsx
@@ -1,12 +1,22 @@
 import React, { FC } from "react";
 import EventViewer from "../views/Events";
 
+import "./Presenter.css";
+import Sidebar from "../components/Sidebar/Sidebar";
+
+
+
+
 interface Props {
 	placeholder?: string;
 }
 
 const EventsPresenter: FC<Props> = () => {
-	return <EventViewer />;
+	return(
+		<div className="mainFlex">
+			<Sidebar/>
+		</div>
+	);
 };
 
 export default EventsPresenter;
--- src/presenters/HomePresenter.tsx
+++ src/presenters/HomePresenter.tsx
@@ -1,12 +1,20 @@
 import React, { FC } from "react";
 import HomeViewer from "../views/Home";
+import Sidebar from "../components/Sidebar/Sidebar";
+
+import "./Presenter.css";
 
 interface Props {
 	placeholder?: string;
 }
 
 const HomePresenter: FC<Props> = () => {
-	return <HomeViewer/>;
+	return (
+		<div className="mainFlex">
+			<Sidebar />
+			<HomeViewer />
+		</div>
+	);
 };
 
 export default HomePresenter;

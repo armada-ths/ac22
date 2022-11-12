77f6fcb1a43a8d2a3849e3c5c5c0d86bedb93d8a
Rakin Ali <rakinali00@gmail.com>
2022-11-12 18:08:38 +0100

    Deleted dead code

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // Import all presenters here
 import HomePresenter from "./presenters/HomePresenter";
-import EventsPresenter from "./presenters/EventsPresenter";
 import RegisterPresenter from "./presenters/RegisterPresenter";
 
 import Login from "./views/Login";
@@ -23,38 +22,48 @@ interface Props {
 }
 
 const App: FC<Props> = ({ userModel }) => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePresenter
-        companies={dummyCompanies}
-              tickets={dummyTickets}
-              onStar={() => console.log("on-star")}/>} />
-        <Route path="/events" element={<EventsPresenter placeholder="events" />} />
-        <Route path="/tutorial" element={<TutorialPresenter />} />
-        <Route path="/createqrcode" element={<CreateQrCodePresenter />} />
-        <Route path="/scanqrcode" element={<ScanQrCodePresenter />} />
-        <Route path="/register" element={<RegisterPresenter />} />
-        <Route
-          path="/overview"
-          element={
-            <AllOrganisationPresenter
-              companies={dummyCompanies}
-              tickets={dummyTickets}
-              onStar={() => console.log("on-star")} />} 
-        />
-        <Route
-          path="/company"
-          element={
-            <CompanyView
-              companies={dummyCompanies}
-              currentCompany={0}
-              onStar={() => console.log("on-star")}
-              availableTickets={dummyTickets} />}
-        />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<HomePresenter
+							companies={dummyCompanies}
+							tickets={dummyTickets}
+							onStar={() => console.log("on-star")}
+						/>
+					}
+				/>
+				<Route path="/tutorial" element={<TutorialPresenter />} />
+				<Route path="/createqrcode" element={<CreateQrCodePresenter />} />
+				<Route path="/scanqrcode" element={<ScanQrCodePresenter />} />
+				<Route path="/register" element={<RegisterPresenter />} />
+				<Route
+					path="/overview"
+					element={
+						<AllOrganisationPresenter
+							companies={dummyCompanies}
+							tickets={dummyTickets}
+							onStar={() => console.log("on-star")}
+						/>
+					}
+				/>
+				<Route
+					path="/company"
+					element={
+						<CompanyView
+							companies={dummyCompanies}
+							currentCompany={0}
+							onStar={() => console.log("on-star")}
+							availableTickets={dummyTickets}
+						/>
+					}
+				/>
+				<Route path="/profile" element={<ProfilePresenter />} />
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;

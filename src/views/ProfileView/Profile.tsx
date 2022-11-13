d664b7bd9751bc42dd8f4f4e62274665d9c12797
Rakin Ali <rakinali00@gmail.com>
2022-11-13 16:56:45 +0100

    More fixing

--- src/views/ProfileView/Profile.tsx
+++ src/views/ProfileView/Profile.tsx
@@ -37,13 +37,31 @@ const ProfileView: FC<Props> = () => {
 			</span>
 			<div className="profile-content-container">
 				{setings ? (
-					/*This is the content of the pages*/ <div className="personalInfo-container">
-						<h3>Personal details</h3>
-						<div className="personal-details-container">
-							<div>
-								<img alt="armada-logo" src={armada_logo} className="roundCircle"></img>
+					/*This is the content of the pages*/
+					<div className="settings-content">
+						<div className="personalInfo-container">
+							<h3>Personal details</h3>
+							<div className="personal-details-container">
+								<div>
+									<img
+										alt="armada-logo"
+										src={armada_logo}
+										className="roundCircle"
+									></img>
+								</div>
+								<div className="name-mail-inputs">
+									<h4>First Name</h4>
+									<input type="text" placeholder="First Name" />
+									<h4>Email</h4>
+									<input type="text" placeholder="Email" />
+								</div>
+								<div className="lastName-gender">
+									<h4>Last Name</h4>
+									<input type="text" placeholder="Last Name" />
+									<h4>Gender</h4>
+									<input type="text" placeholder="Gender" />
+								</div>
 							</div>
-							
 						</div>
 					</div>
 				) : (

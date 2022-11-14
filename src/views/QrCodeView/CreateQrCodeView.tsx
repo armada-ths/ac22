2905de6c92d7f7b1ea40a9fcbc3f9afd7dab732d
Yash Dhanore <yashdhanore@gmail.com>
2022-11-14 11:00:11 +0100

    fix mobile view, need to add side options

--- src/views/QrCodeView/CreateQrCodeView.tsx
+++ src/views/QrCodeView/CreateQrCodeView.tsx
@@ -1,12 +1,11 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import QRCode from "react-qr-code";
 import "./CreateQRCodeView.css";
 import logo from "../../assets/Armada_competition_filled.png";
 import AuthButton from "../../components/AuthButton/AuthButton";
 import ticket_img from "../../assets/TicketState/ticket-img.svg";
 import plus_sign_img from "../../assets/TicketState/plus-sign-img.svg";
-import ticket_grey_img from "../../assets/TicketState/ticket-grey-img.svg";
-import plus_sign_grey_img from "../../assets/TicketState/plus-sign-grey-img.svg";
+import useCheckMobileScreen from "../../useCheckMobileScreen";
 
 interface Props {
   setTicketType: (ticketType: string) => void;
@@ -26,115 +25,270 @@ const CreateQrCodeView: FC<Props> = (props) => {
     props.generateURL();
   };
 
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  // const [popup, SetPopup] = useState(false);
+
   return (
-    <div className="full-screen">
-      <div className="side">
-        <img src={logo} alt="logo" className="logo" />
-        <div className="button-container">
-          <div className="buttons">
-            <AuthButton
-              buttonText="Home"
-              active={true}
-              buttonType={"button"}
-              onButtonClick={() => console.log("hey")}
-            />
-            <AuthButton
-              buttonText="How to play"
-              active={true}
-              buttonType={"button"}
-              onButtonClick={() => console.log("hey")}
-            />
-            <AuthButton
-              buttonText="Statistics"
-              active={true}
-              buttonType={"button"}
-              onButtonClick={() => console.log("hey")}
-            />
-            <AuthButton
-              buttonText="Log out"
-              active={true}
-              buttonType={"button"}
-              onButtonClick={() => console.log("hey")}
-            />
-          </div>
-        </div>
-        <img src={logo} alt="logo" className="logo" />
-        <div></div>
-      </div>
-      <div className="main-content">
-        <div className="company-name">Generate QRCode for {props.company}</div>
-        <div className="ticket">
-          <select
-            className="select-option"
-            name="tickettype"
-            onChange={(e) => {
-              props.setTicketType(e.target.value);
-              props.setIsShown(false);
-            }}
-          >
-            <option value="standardticket">Standard Ticket</option>
-            <option value="superticket">Super Ticket</option>
-          </select>
-          <AuthButton
-            onButtonClick={handleClick}
-            buttonText="Create Ticket"
-            buttonType="button"
-            active={true}
-          ></AuthButton>
-          {/* <button onClick={handleClick}>Create Ticket</button> */}
-        </div>
-
-        <div id="qrCodeContainer">
-          {props.isShown && (
-            <div className="qr-container">
-              <QRCode
-                size={256}
-                style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-                value={props.qrCode}
-                viewBox={`0 0 256 256`}
-              />
-
-              {props.ticketType === "superticket" ? (
-                <div className="left-box">
-                  <div className="ticket-logo">
-                    <img
-                      className="tckt-img"
-                      src={ticket_img}
-                      alt={"ticket available"}
-                    ></img>
-                    <img
-                      className="ticket-plus-sign"
-                      src={plus_sign_img}
-                      alt={"plus sign available"}
-                    ></img>
-                  </div>
-                </div>
-              ) : (
-                ""
-              )}
-
-              <h3>
-                {props.ticketType === "standardticket"
-                  ? "Standard Ticket"
-                  : "Super Ticket"}
-              </h3>
-
-              <div>
+    <div>
+      {useCheckMobileScreen() ? (
+        <div className="full-screen">
+          {/* <div className="header"></div> */}
+          <div className="main-content">
+            <div className="company-name">
+              Generate QRCode for {props.company}
+            </div>
+            <div className="ticket">
+              <select
+                className="select-option"
+                name="tickettype"
+                onChange={(e) => {
+                  props.setTicketType(e.target.value);
+                  props.setIsShown(false);
+                }}
+              >
+                <option value="standardticket">Standard Ticket</option>
+                <option value="superticket">Super Ticket</option>
+              </select>
+              <div className="mobile-button">
                 <AuthButton
-                  onButtonClick={() => {
-                    props.removePreviousQrCode();
-                    props.setIsShown(false);
-                  }}
-                  buttonText="Remove QRCode"
+                  onButtonClick={handleClick}
+                  buttonText="Create Ticket"
                   buttonType="button"
                   active={true}
                 ></AuthButton>
-                <h5>Only use this if ticket has not yet been scanned!</h5>
               </div>
             </div>
-          )}
+            <div id="qrCodeContainer">
+              {props.isShown && (
+                <div className="qr-container">
+                  <QRCode
+                    size={256}
+                    style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                    value={props.qrCode}
+                    viewBox={`0 0 256 256`}
+                  />
+
+                  {props.ticketType === "superticket" ? (
+                    <div className="left-box">
+                      <div className="ticket-logo">
+                        <img
+                          className="tckt-img"
+                          src={ticket_img}
+                          alt={"ticket available"}
+                        ></img>
+                        <img
+                          className="ticket-plus-sign"
+                          src={plus_sign_img}
+                          alt={"plus sign available"}
+                        ></img>
+                      </div>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+
+                  <h3>
+                    {props.ticketType === "standardticket"
+                      ? "Standard Ticket"
+                      : "Super Ticket"}
+                  </h3>
+
+                  <div>
+                    <AuthButton
+                      onButtonClick={() => {
+                        props.removePreviousQrCode();
+                        props.setIsShown(false);
+                      }}
+                      buttonText="Remove QRCode"
+                      buttonType="button"
+                      active={true}
+                    ></AuthButton>
+                    <h5>Only use this if ticket has not yet been scanned!</h5>
+                  </div>
+                </div>
+              )}
+            </div>
+            <button onClick={openModal}>Open Modal</button>
+          </div>
+          {/* <div className="main-content">
+            <div className="company-name">
+              Generate QRCode for {props.company}
+            </div>
+            
+
+            <div id="qrCodeContainer">
+              {props.isShown && (
+                <div className="qr-container">
+                  <QRCode
+                    size={256}
+                    style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                    value={props.qrCode}
+                    viewBox={`0 0 256 256`}
+                  />
+
+                  {props.ticketType === "superticket" ? (
+                    <div className="left-box">
+                      <div className="ticket-logo">
+                        <img
+                          className="tckt-img"
+                          src={ticket_img}
+                          alt={"ticket available"}
+                        ></img>
+                        <img
+                          className="ticket-plus-sign"
+                          src={plus_sign_img}
+                          alt={"plus sign available"}
+                        ></img>
+                      </div>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+
+                  <h3>
+                    {props.ticketType === "standardticket"
+                      ? "Standard Ticket"
+                      : "Super Ticket"}
+                  </h3>
+
+                  <div>
+                    <AuthButton
+                      onButtonClick={() => {
+                        props.removePreviousQrCode();
+                        props.setIsShown(false);
+                      }}
+                      buttonText="Remove QRCode"
+                      buttonType="button"
+                      active={true}
+                    ></AuthButton>
+                    <h5>Only use this if ticket has not yet been scanned!</h5>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div> */}
         </div>
-      </div>
+      ) : (
+        <div className="full-screen">
+          <div className="side">
+            <img src={logo} alt="logo" className="logo" />
+            <div className="button-container">
+              <div className="buttons">
+                <AuthButton
+                  buttonText="Home"
+                  active={true}
+                  buttonType={"button"}
+                  onButtonClick={() => console.log("hey")}
+                />
+                <AuthButton
+                  buttonText="How to play"
+                  active={true}
+                  buttonType={"button"}
+                  onButtonClick={() => console.log("hey")}
+                />
+                <AuthButton
+                  buttonText="Statistics"
+                  active={true}
+                  buttonType={"button"}
+                  onButtonClick={() => console.log("hey")}
+                />
+                <AuthButton
+                  buttonText="Log out"
+                  active={true}
+                  buttonType={"button"}
+                  onButtonClick={() => console.log("hey")}
+                />
+              </div>
+            </div>
+            <img src={logo} alt="logo" className="logo" />
+            <div></div>
+          </div>
+          <div className="main-content">
+            <div className="company-name">
+              Generate QRCode for {props.company}
+            </div>
+            <div className="ticket">
+              <select
+                className="select-option"
+                name="tickettype"
+                onChange={(e) => {
+                  props.setTicketType(e.target.value);
+                  props.setIsShown(false);
+                }}
+              >
+                <option value="standardticket">Standard Ticket</option>
+                <option value="superticket">Super Ticket</option>
+              </select>
+              <AuthButton
+                onButtonClick={handleClick}
+                buttonText="Create Ticket"
+                buttonType="button"
+                active={true}
+              ></AuthButton>
+              {/* <button onClick={handleClick}>Create Ticket</button> */}
+            </div>
+
+            <div id="qrCodeContainer">
+              {props.isShown && (
+                <div className="qr-container">
+                  <QRCode
+                    size={256}
+                    style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                    value={props.qrCode}
+                    viewBox={`0 0 256 256`}
+                  />
+
+                  {props.ticketType === "superticket" ? (
+                    <div className="left-box">
+                      <div className="ticket-logo">
+                        <img
+                          className="tckt-img"
+                          src={ticket_img}
+                          alt={"ticket available"}
+                        ></img>
+                        <img
+                          className="ticket-plus-sign"
+                          src={plus_sign_img}
+                          alt={"plus sign available"}
+                        ></img>
+                      </div>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+
+                  <h3>
+                    {props.ticketType === "standardticket"
+                      ? "Standard Ticket"
+                      : "Super Ticket"}
+                  </h3>
+
+                  <div>
+                    <AuthButton
+                      onButtonClick={() => {
+                        props.removePreviousQrCode();
+                        props.setIsShown(false);
+                      }}
+                      buttonText="Remove QRCode"
+                      buttonType="button"
+                      active={true}
+                    ></AuthButton>
+                    <h5>Only use this if ticket has not yet been scanned!</h5>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };

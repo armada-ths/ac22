0b30e4f82b2a011ab5322a8cf3bb3b9f4412eac1
Adeel Hussain <adeelh@kth.se>
2022-11-17 01:30:36 +0100

    Added check for amount of superticket left to generate + formatting
    Companies can now create a limited amount of tickets (this checks from the database of the company value SuperTicketsLeft).
    
    Fixed some formatting for few pages
    
    Changed redirectURL to fetch from browser URL instead of having fixed

--- src/models/Firebase/FirebaseModel.tsx
+++ src/models/Firebase/FirebaseModel.tsx
@@ -1,127 +1,150 @@
 import {
-  collection,
-  doc,
-  getDoc,
-  getDocs,
-  setDoc,
-  increment,
-  updateDoc,
-  deleteField,
+	collection,
+	doc,
+	getDoc,
+	getDocs,
+	setDoc,
+	increment,
+	updateDoc,
+	deleteField,
 } from "firebase/firestore";
 import { database } from "./firebaseConfig";
 
 export async function addToDB(
-  collectionName: string,
-  documentID: any,
-  data: any
+	collectionName: string,
+	documentID: any,
+	data: any
 ) {
-  const docRef = doc(database, collectionName, documentID);
-  if ((await getDoc(docRef)).exists()) {
-    switch (collectionName) {
-      case "users":
-        //Add to users
-        break;
+	const docRef = doc(database, collectionName, documentID);
+	if ((await getDoc(docRef)).exists()) {
+		switch (collectionName) {
+			case "users":
+				//Add to users
+				break;
 
-      case "companies":
-        addToCompanyDatabase(documentID, data.ticketType, data.ticketNr);
-        break;
-    }
-  } else {
-    try {
-      await setDoc(docRef, {
-        TotalTickets: 0,
-        TotalTicketsLeft: 0,
-      });
-    } catch (e) {}
-    addToDB(collectionName, documentID, data);
-  }
+			case "companies":
+				addToCompanyDatabase(documentID, data.ticketType, data.ticketNr);
+				break;
+		}
+	} else {
+		try {
+			await setDoc(docRef, {
+				TotalTickets: 0,
+				TotalTicketsLeft: 0,
+			});
+		} catch (e) {}
+		addToDB(collectionName, documentID, data);
+	}
 }
 
 export async function addToCompanyDatabase(
-  company: string,
-  ticketType: string,
-  ticketNr: number
+	company: string,
+	ticketType: string,
+	ticketNr: number
 ) {
-  const docRef = doc(database, "companies", company);
-  const ticket = "Ticket " + ticketNr;
-  await updateDoc(docRef, {
-    TotalTickets: increment(1),
-    TotalTicketsLeft: increment(1),
-    [ticket]: {
-      ticketType: ticketType,
-      available: true,
-    },
-  });
+	const docRef = doc(database, "companies", company);
+	const ticket = "Ticket " + ticketNr;
+	console.log(ticketType);
+	if (ticketType === "superticket") {
+		await updateDoc(docRef, {
+			SuperTicketsLeft: increment(-1),
+			TotalTickets: increment(1),
+			TotalTicketsLeft: increment(1),
+			[ticket]: {
+				ticketType: ticketType,
+				available: true,
+			},
+		});
+		console.log("superTicket--");
+	} else {
+		await updateDoc(docRef, {
+			TotalTickets: increment(1),
+			TotalTicketsLeft: increment(1),
+			[ticket]: {
+				ticketType: ticketType,
+				available: true,
+			},
+		});
+	}
 }
 
 export async function claimTicket(company: string, ticketNr: number) {
-  const ticket = "Ticket " + ticketNr;
-  const docRef = doc(database, "companies", company);
-  const docSnap = await getDoc(docRef);
-  if (docSnap.exists()) {
-    const data = docSnap.data();
-    if (data[ticket].available) {
-      await setDoc(
-        docRef,
-        {
-          [ticket]: {
-            available: false,
-          },
-          TotalTicketsLeft: increment(-1),
-        },
-        { merge: true }
-      );
-    } else {
-    }
-  }
+	const ticket = "Ticket " + ticketNr;
+	const docRef = doc(database, "companies", company);
+	const docSnap = await getDoc(docRef);
+	if (docSnap.exists()) {
+		const data = docSnap.data();
+		if (data[ticket].available) {
+			await setDoc(
+				docRef,
+				{
+					[ticket]: {
+						available: false,
+					},
+					TotalTicketsLeft: increment(-1),
+				},
+				{ merge: true }
+			);
+		} else {
+		}
+	}
 }
 
 export async function addToUserDatabase(
-  user: string,
-  starredCompanies: any,
-  collectedTickets: any
+	user: string,
+	starredCompanies: any,
+	collectedTickets: any
 ) {
-  const docRef = doc(database, "users", user);
+	const docRef = doc(database, "users", user);
 
-  try {
-    await setDoc(docRef, {
-      starredCompanies: starredCompanies,
-      collectedTickets: collectedTickets,
-    });
-  } catch (e) {}
+	try {
+		await setDoc(docRef, {
+			starredCompanies: starredCompanies,
+			collectedTickets: collectedTickets,
+		});
+	} catch (e) {}
 }
 
 export async function getCompanyData(company: string) {
-  const docRef = doc(database, "companies", company);
-  const docSnap = await getDoc(docRef);
-  if (docSnap.exists()) {
-    return docSnap.data();
-  } else {
-  }
+	const docRef = doc(database, "companies", company);
+	const docSnap = await getDoc(docRef);
+	if (docSnap.exists()) {
+		return docSnap.data();
+	} else {
+	}
 }
 
 export async function removeFromDB(
-  collectionName: string,
-  documentID: any,
-  data: any
+	collectionName: string,
+	documentID: any,
+	data: any
 ) {
-  const docRef = doc(database, collectionName, documentID);
-  try {
-    if (collectionName === "companies" && data.includes("Ticket")) {
-      await updateDoc(docRef, {
-        [data]: deleteField(),
-        TotalTickets: increment(-1),
-        TotalTicketsLeft: increment(-1),
-      });
-    }
-  } catch (e) {
-    console.error("Error removing document: ", e);
-  }
+	const docRef = doc(database, collectionName, documentID);
+	try {
+		if (collectionName === "companies" && data.includes("Ticket")) {
+			if (data.includes("superticket")) {
+				await updateDoc(docRef, {
+					[data]: deleteField(),
+					TotalTickets: increment(-1),
+					TotalTicketsLeft: increment(-1),
+					SuperTicketsLeft: increment(1),
+				});
+			} else {
+				await updateDoc(docRef, {
+					[data]: deleteField(),
+					TotalTickets: increment(-1),
+					TotalTicketsLeft: increment(-1),
+				});
+			}
+		}
+	} catch (e) {
+		console.error("Error removing document: ", e);
+	}
 }
 
 export async function fetchFromDatabase(dbCollection: string) {
-  const querySnapshot = await getDocs(collection(database, dbCollection));
-  querySnapshot.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
-  });
+	const querySnapshot = await getDocs(collection(database, dbCollection));
+	querySnapshot.forEach((doc) => {
+		// doc.data() is never undefined for query doc snapshots
+	});
 }

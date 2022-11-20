03af5b09ecb7a60b871a41388a89713e4be2c50f
Yash Dhanore <yashdhanore@gmail.com>
2022-11-21 00:58:09 +0100

    goes back from scanqrcode

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import NavBarTicket from "../NavBarTicket/NavBarTicket";
 import qrImg from "../../assets/NavBar/qrcode-img.svg";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import "./NavBar.css";
 
 interface Props {
@@ -10,6 +10,7 @@ interface Props {
   title: String;
   qrButtonActive: boolean;
 }
+
 const NavBar: FC<Props> = ({
   name,
   title,
@@ -21,17 +22,25 @@ const NavBar: FC<Props> = ({
     document.documentElement.scrollTop = 0;
   }
 
+  const location = useLocation();
+  const navigate = useNavigate();
+
   return (
     <div className="navbar-base">
-      <h1 onClick={() => toTop()}>{title}</h1>
+      <h1
+        onClick={() =>
+          location.pathname === "/scanqrcode" ? navigate(-1) : toTop()
+        }
+      >
+        {title}
+      </h1>
       <div className="navbar-right">
         <NavBarTicket collectedTickets={collectedTickets} />
         <Link
           to="/scanqrcode"
           className={qrButtonActive ? "navbar-qr" : "navbar-qr-hide"}
         >
-          {" "}
-          <img src={qrImg} />
+          <img src={qrImg} alt="qr" />
         </Link>
         <Link to="/profile" className="navbar-name">
           {name[1]}

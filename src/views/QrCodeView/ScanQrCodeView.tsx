9b87538c8afc75a96eebeaf0efcaf11e2721a054
Yash Dhanore <yashdhanore@gmail.com>
2022-11-21 09:43:19 +0100

    add back icon and turn off camera when not in use

--- src/views/QrCodeView/ScanQrCodeView.tsx
+++ src/views/QrCodeView/ScanQrCodeView.tsx
@@ -8,6 +8,7 @@ import { TextResult } from "dynamsoft-javascript-barcode";
 import { LoadingIcon } from "../../assets/LoadingIcon/Loading";
 import { InvalidIcon } from "../../assets/Invalid/InvalidIcon";
 import { auth } from "../../models/Firebase/firebaseConfig";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   company: string;
@@ -20,8 +21,8 @@ const ScanQrCodeView: FC<Props> = (props) => {
   const [ticketStatus, setTicketStatus] = useState(true);
   const [Initialized, setInitialized] = useState(false);
   const [isActive, setIsActive] = useState(true); //whether the camera is active
-  const [isPause, setIsPause] = useState(false); //whether the video is paused
-  const [runtimeSettings, setRuntimeSettings] = useState(
+  const [isPause] = useState(false); //whether the video is paused
+  const [runtimeSettings] = useState(
     '{"ImageParameter":{"BarcodeFormatIds":["BF_QR_CODE"],"Description":"","Name":"Settings"},"Version":"3.0"}'
   ); //use JSON template to decode QR codes only
 
@@ -37,6 +38,8 @@ const ScanQrCodeView: FC<Props> = (props) => {
     }
   }
 
+  const navigate = useNavigate();
+
   const onInitialized = () => {
     // when the Barcode Reader is initialized
     setInitialized(true);
@@ -45,9 +48,27 @@ const ScanQrCodeView: FC<Props> = (props) => {
   return (
     <div>
       <div className="scan-navbar">
+        <svg
+          className="scan-back-button"
+          onClick={() => {
+            setIsActive(false);
+            navigate(-1);
+          }}
+          width="52"
+          height="31"
+          viewBox="0 0 52 31"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M50 13.5C51.1046 13.5 52 14.3954 52 15.5C52 16.6046 51.1046 17.5 50 17.5V13.5ZM0.585785 16.9142C-0.195263 16.1332 -0.195263 14.8668 0.585785 14.0858L13.3137 1.35786C14.0948 0.576816 15.3611 0.576816 16.1421 1.35786C16.9232 2.13891 16.9232 3.40524 16.1421 4.18629L4.82843 15.5L16.1421 26.8137C16.9232 27.5948 16.9232 28.8611 16.1421 29.6421C15.3611 30.4232 14.0948 30.4232 13.3137 29.6421L0.585785 16.9142ZM50 17.5H2V13.5H50V17.5Z"
+            fill="#0F1322"
+          />
+        </svg>
+
         <NavBar
           name={["", auth.currentUser?.displayName as string]}
-          title="<- Back"
+          title=""
           collectedTickets={10}
           qrButtonActive={false}
         />
@@ -108,7 +129,7 @@ const ScanQrCodeView: FC<Props> = (props) => {
         ) : (
           ""
         )}
-        <div className="scan-text">Scan QR Code to collect your ticket</div>
+        <div className="scan-text">Scan the QR Code to collect your ticket</div>
       </div>
     </div>
   );

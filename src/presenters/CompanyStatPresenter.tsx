853de5948621106190961eb401105d1eb362f070
Anton Danker <anton.danker@gmail.com>
2022-11-28 16:07:08 +0100

    Improved company name matching for statistics

--- src/presenters/CompanyStatPresenter.tsx
+++ src/presenters/CompanyStatPresenter.tsx
@@ -2,26 +2,20 @@ import React, { FC } from "react";
 import { CompanyUserModel } from "../models/companyUserModel";
 import CompanyStatView from "../views/CompanyStatView/CompanyStatView";
 
-
 interface props {
-    user: CompanyUserModel;
+  user: CompanyUserModel;
 }
 
+const CompanyStatPresenter: FC<props> = ({ user }) => {
+  const [company, setCompany] = React.useState(user.email as string);
 
-const CompanyStatPresenter: FC<props> = ({user}) => {
-    const [company, setCompany] = React.useState(user.email as string); 
-    
-    function sanitize(string: string) {
-		return (
-			string.charAt(0).toUpperCase() +
-			string.substring(0, string.indexOf("@")).slice(1)
-		);
-	}
+  function sanitize(string: string) {
     return (
-        <CompanyStatView 
-        company = {sanitize(company)}
-        />
+      string.charAt(0).toUpperCase() +
+      string.substring(0, string.indexOf("@")).slice(1)
     );
-  };
+  }
+  return <CompanyStatView company={sanitize(company)} />;
+};
 
 export default CompanyStatPresenter;
